c19a54bf1dbd46c9de630636f4b00b7766858c06
yymmm9 <46626991+yymmm9@users.noreply.github.com>
2024-01-09 16:58:13 +0100

    Update table-csv.js

--- table-csv.js
+++ table-csv.js
@@ -1,133 +1,136 @@
-const button = document.createElement("button");
-button.id = "btnExport";
-button.type = "button";
-button.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-down"><path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/><polyline points="14 2 14 8 20 8"/><path d="M12 18v-6"/><path d="m9 15 3 3 3-3"/></svg>`
-button.appendChild(document.createTextNode(" Export to CSV"));
 
-document.body.appendChild(button);
+document.addEventListener('DOMContentLoaded', function () {
 
-class CsvExport {
-  constructor(table, header = true) {
-    this.table = table;
-    this.rows = Array.from(table.querySelectorAll("tr"));
-    if (!header && this.rows[0].querySelectorAll("th").length) {
-      this.rows.shift();
-    }
-  }
+  const button = document.createElement("button");
+  button.id = "btnExport";
+  button.type = "button";
+  button.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-down"><path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/><polyline points="14 2 14 8 20 8"/><path d="M12 18v-6"/><path d="m9 15 3 3 3-3"/></svg>`
+  button.appendChild(document.createTextNode(" Export to CSV"));
 
-  exportCsv() {
-    const lines = this.rows.map((row) =>
-      Array.from(row.children)
-        .map((cell) => CsvExport.safeData(cell, textRemovalRules))
-        .join("\t")
-    );
-    return lines.join("\n");
-  }
+  document.body.appendChild(button);
 
-  static safeData(td, textRemovalRules) {
-    let data = td.textContent;
-    let regex = /(\d{0,14}[A-Z]?)(?=\s?\(EAN\))/;
-
-    if (regex.test(data)) {
-      data = data.match(regex)[1];
-    }
-
-    textRemovalRules.forEach((rule) => {
-      if (rule.type === 1) {
-        data = data.replace(new RegExp(escapeRegExp(rule.text), 'g'), '');
-      } else if (rule.type === 2) {
-        const flags = rule.caseInsensitive ? 'gi' : 'g';
-        data = data.replace(new RegExp(`${escapeRegExp(rule.text)}.*$`, flags), '');
-      }
-    });
-    // data = data.replace(/,/g, ",");
-    return data;
-  }
-}
-
-const btnExport = document.querySelector("#btnExport");
-
-// 1: complete, 2: fromItsPosition
-const textRemovalRules = [
-  { type: 1, text: "(AswArtFor)" },
-  { type: 2, text: "Lotto:" },
-  { type: 2, text: "Tipo Dato:", caseInsensitive: true },
-  { type: 2, text: "Vs.Ord. O/", caseInsensitive: true },
-  { extractEAN: true },
-];
-function escapeRegExp(string) {
-  return string.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
-}
-
-function download_table_as_csv(table_id) {
-  // Select rows from table_id
-  var rows = document.querySelectorAll('' + table_id + ' tr');
-  // Construct csv
-  var csv = [];
-  for (var i = 0; i < rows.length; i++) {
-    var row = [],
-      cols = rows[i].querySelectorAll('td, th');
-    var rowIsEmpty = true; // Flag to check if the row is empty
-    for (var j = 0; j < cols.length; j++) {
-      // Clean innertext and apply text removal rules
-      var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ');
-      data = CsvExport.safeData({ textContent: data }, textRemovalRules);
-      // Escape commas by wrapping the field in double quotes
-      if (data.includes(',') || data.includes('\n')) {
-        data = `"${data.replace(/"/g, '""')}"`;
-      }
-      row.push(data);
-      // Check if the cell contains non-whitespace content
-      if (data.trim() !== '') {
-        rowIsEmpty = false;
+  class CsvExport {
+    constructor(table, header = true) {
+      this.table = table;
+      this.rows = Array.from(table.querySelectorAll("tr"));
+      if (!header && this.rows[0].querySelectorAll("th").length) {
+        this.rows.shift();
       }
     }
 
-    if (!rowIsEmpty) {
-      csv.push(row.join(","));
+    exportCsv() {
+      const lines = this.rows.map((row) =>
+        Array.from(row.children)
+          .map((cell) => CsvExport.safeData(cell, textRemovalRules))
+          .join("\t")
+      );
+      return lines.join("\n");
+    }
+
+    static safeData(td, textRemovalRules) {
+      let data = td.textContent;
+      let regex = /(\d{0,14}[A-Z]?)(?=\s?\(EAN\))/;
+
+      if (regex.test(data)) {
+        data = data.match(regex)[1];
+      }
+
+      textRemovalRules.forEach((rule) => {
+        if (rule.type === 1) {
+          data = data.replace(new RegExp(escapeRegExp(rule.text), 'g'), '');
+        } else if (rule.type === 2) {
+          const flags = rule.caseInsensitive ? 'gi' : 'g';
+          data = data.replace(new RegExp(`${escapeRegExp(rule.text)}.*$`, flags), '');
+        }
+      });
+      // data = data.replace(/,/g, ",");
+      return data;
     }
   }
-  var csv_string = csv.join('\n');
-  // Download it
-  var filename = 'ev7_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
-  var link = document.createElement('a');
-  link.style.display = 'none';
-  link.setAttribute('target', '_blank');
-  link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
-  link.setAttribute('download', filename);
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
+
+  const btnExport = document.querySelector("#btnExport");
+
+  // 1: complete, 2: fromItsPosition
+  const textRemovalRules = [
+    { type: 1, text: "(AswArtFor)" },
+    { type: 2, text: "Lotto:" },
+    { type: 2, text: "Tipo Dato:", caseInsensitive: true },
+    { type: 2, text: "Vs.Ord. O/", caseInsensitive: true },
+    { extractEAN: true },
+  ];
+  function escapeRegExp(string) {
+    return string.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
+  }
+
+  function download_table_as_csv(table_id) {
+    // Select rows from table_id
+    var rows = document.querySelectorAll('' + table_id + ' tr');
+    // Construct csv
+    var csv = [];
+    for (var i = 0; i < rows.length; i++) {
+      var row = [],
+        cols = rows[i].querySelectorAll('td, th');
+      var rowIsEmpty = true; // Flag to check if the row is empty
+      for (var j = 0; j < cols.length; j++) {
+        // Clean innertext and apply text removal rules
+        var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ');
+        data = CsvExport.safeData({ textContent: data }, textRemovalRules);
+        // Escape commas by wrapping the field in double quotes
+        if (data.includes(',') || data.includes('\n')) {
+          data = `"${data.replace(/"/g, '""')}"`;
+        }
+        row.push(data);
+        // Check if the cell contains non-whitespace content
+        if (data.trim() !== '') {
+          rowIsEmpty = false;
+        }
+      }
+
+      if (!rowIsEmpty) {
+        csv.push(row.join(","));
+      }
+    }
+    var csv_string = csv.join('\n');
+    // Download it
+    var filename = 'ev7_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
+    var link = document.createElement('a');
+    link.style.display = 'none';
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 
 
-btnExport.addEventListener("click", () => {
-  const container = document.getElementById('fattura-elettronica');
-  if (container) {
-    // Get all tables inside the container
-    const tables = container.getElementsByTagName('table');
+  btnExport.addEventListener("click", () => {
+    const container = document.getElementById('fattura-elettronica');
+    if (container) {
+      // Get all tables inside the container
+      const tables = container.getElementsByTagName('table');
 
-    // Check if the container has at least the minimum number of tables
-    if (tables.length >= 9) {
-      console.log('Container has ' + tables.length + ' tables.');
-      download_table_as_csv("#fattura-elettronica table:nth-child(7)")
+      // Check if the container has at least the minimum number of tables
+      if (tables.length >= 9) {
+        console.log('Container has ' + tables.length + ' tables.');
+        download_table_as_csv("#fattura-elettronica table:nth-child(7)")
+      } else {
+        console.log('Container does not have enough tables.', tables.length);
+        download_table_as_csv("#fattura-elettronica table:nth-child(5)")
+      }
     } else {
-      console.log('Container does not have enough tables.', tables.length);
-      download_table_as_csv("#fattura-elettronica table:nth-child(5)")
+      console.error('Container not found.');
     }
-  } else {
-    console.error('Container not found.');
+  });
+
+
+  function addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
   }
-});
 
-
-function addStyle(styleString) {
-  const style = document.createElement('style');
-  style.textContent = styleString;
-  document.head.append(style);
-}
-
-addStyle(`#btnExport {
+  addStyle(`#btnExport {
 display: flex;
 align-items: center;
 justify-items: center;
@@ -155,3 +158,5 @@ font-size: 1rem;
       #fattura-elettronica table:nth-child(7) {
         /* background: red; */
       }`)
+
+}, false);
